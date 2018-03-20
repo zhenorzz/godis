f554fb732729251b45356c11660aa7ea6d03f38f
Zane <liz@uclbrt.com>
2018-03-20 22:32:04 +0800

    simple key-value

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+	"strings"
+	"io/ioutil"
 )
 
+var cache map[string]string
+
 func main() {
-	fmt.Println(1)
+	cache = make(map[string]string)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		uri := r.RequestURI
+		pathInfo := strings.Split(uri, "/")
+		switch r.Method {
+		case "POST":
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				panic(err)
+			}
+			value := string(body)
+			cache[pathInfo[2]] = value
+			break
+		case "GET":
+			fmt.Fprintf(w, "%s : %s", pathInfo[2], cache[pathInfo[2]])
+			break
+		}
+
+		fmt.Println(r.Method, pathInfo)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
