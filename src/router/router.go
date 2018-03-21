5ae3e9689277d7a55225d28f330ac7d819faeec1
zhangzhen <zhangzhen@tanwan.com>
2018-03-21 13:52:05 +0800

    post and get

--- src/router/router.go
+++ src/router/router.go
@@ -9,37 +9,55 @@ import (
 )
 
 type Cache map[string]string
-func (cache Cache)Router(w http.ResponseWriter, r *http.Request) {
-	uri := r.RequestURI
-	pathInfo := strings.Split(uri, "/")
+
+func (cache Cache) Router(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		var response map[string]string
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			panic(err)
-		}
-		err = json.Unmarshal(body, &response)
-		if err != nil {
-			panic(err)
-		}
-		if len(response) == 0 {
-			fmt.Fprintf(w, "%v", false)
-			return
-		}
-		for k, v := range response {
-			if _, ok := cache[k]; ok {
-				fmt.Fprintf(w, "%s al ready exist", k)
-				return
-			}
-			cache[k] = v
-		}
+		result := cache.post(r)
+		fmt.Fprint(w, result)
 		fmt.Println(cache)
 		break
 	case "GET":
-		fmt.Fprintf(w, "%s : %s", pathInfo[1], cache[pathInfo[1]])
+		result := cache.get(r)
+		fmt.Fprint(w, result)
+		fmt.Println(cache)
 		break
 	}
 
-	fmt.Println(r.Method, pathInfo)
+}
+
+func (cache Cache) post(r *http.Request) string {
+	var response map[string]string
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		panic(err)
+	}
+	if len(response) == 0 {
+		return "none json in body"
+	}
+	for k, v := range response {
+		if _, ok := cache[k]; ok {
+			return fmt.Sprintf("%s already exist", k)
+		}
+		cache[k] = v
+	}
+	return "success"
+}
+
+func (cache Cache) get(r *http.Request) string{
+	uri := r.RequestURI
+	pathInfo := strings.Split(uri, "/")
+	if len(pathInfo) == 0 {
+		return "no path info"
+	}
+	key := pathInfo[1]
+	if _, ok := cache[key]; ok {
+		return cache[key]
+	} else {
+		return fmt.Sprintf("%s is not exist", key)
+	}
 }
